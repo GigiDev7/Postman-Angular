4a30d3ab4a239a8194defe4c4105fb48bee73f82
GigiDev7 <gigivadachkoria@gmail.com>
2022-08-02 16:52:12 +0400

    initial setup, basic home page setup with styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'postman-angular';
+  public activeParam: string = 'Query Params';
+
+  public onActiveParamChange = (val: string) => {
+    this.activeParam = val;
+  };
+
+  constructor() {}
 }
